ed7937113ad71cc93b8e7d76505abec4e665fbf9
AlexPuente <eljudicador6@gmail.com>
2013-11-16 19:07:48 -0500

    Version Alex
    ddd

---
+++ PruebaAlex/Templates/PlantillaBase.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Holas</title>
+<link href="../Estilos/EstilosPrincipal.css" rel="stylesheet" type="text/css" />
+<link  href="../Estilos/menu.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+<?php include("../includes/menu.php"); ?>
+
+
+  
+  
+ <div class="MenuJuntas">
+ 
+    <ul >
+
+      <li><a href="https://www.google.com.pe">CREAR JUNTAS</a></li>
+      <li><a href="https://www.google.com.pe">MIS JUNTAS</a></li>
+      <li><a href="https://www.google.com.pe">INVITACION</a></li>
+      
+    </ul>
+ </div>
+  
+<div class="Video">
+ 
+ <iframe  src="//www.youtube.com/embed/EsIwvilcdbA?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+
+   </div>
+   <div class="Imagen">
+  <img src="../Imagenes/Holi.jpg"   >
+  </div>
+</body>
+</html>
