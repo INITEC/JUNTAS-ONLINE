6b4a2a7049e003ea29af93edd8a78ef613f446dc
JOTAI <jotai.bf.2012@gmail.com>
2013-11-21 02:52:45 -0500

    crear junta
    se crea una nueva junta

---
+++ Templates/crear_junta.php
@@ -0,0 +1,15 @@
+<?php
+session_start(); 
+require_once("../require/class.php");
+
+?>
+
+<html>
+<head>
+<title>>>Nueva Junta<<</title>
+</head>
+
+<body>
+<h1>Creando Nueva Junta</h1>
+</body>
+</html>
