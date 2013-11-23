e459e0d8d125ae8e3db8a723675d84b97f619d9d
JOTAI <jotai.bf.2012@gmail.com>
2013-11-23 14:26:19 -0500

    crear junta
    partes para la ventana crear junta, incompleto v1

--- Templates/crear_junta.php
+++ Templates/crear_junta.php
@@ -1,15 +1,25 @@
 <?php
 session_start(); 
 require_once("../require/class.php");
+include ("../includes/slider_horizontal.php");
 
 ?>
 
 <html>
 <head>
 <title>>>Nueva Junta<<</title>
-</head>
+<script type="text/javascript" src="../JavaScript/range.js"></script>
+<script type="text/javascript" src="../JavaScript/timer.js"></script>
+<script type="text/javascript" src="../JavaScript/slider.js"></script>
+<link type="text/css" rel="StyleSheet" href="../Estilos/bluecurve/bluecurve.css" />
 
+</head>
 <body>
 <h1>Creando Nueva Junta</h1>
+
+<?php colocar_slider_horizontal("1",0,100,1); ?>
+
+
+
 </body>
 </html>
---
+++ require/slider_horizontal.php
@@ -0,0 +1,31 @@
+<?php 
+function colocar_slider_horizontal($title,$max,$min,$incremento) {
+
+?>
+
+<p><?php echo $title; ?></p>
+<div class="slider" id="slider-<?php echo $title; ?>" tabIndex="<?php echo $title; ?>">
+	<input class="slider-input" id="slider-input-<?php echo $title; ?>"/>
+</div>
+
+<p>
+Value: <input id="h-value_<?php echo $title; ?>" onchange="s_<?php echo $title; ?>.setValue(parseInt(this.value))"/>
+Minimum: <input id="h-min_<?php echo $title; ?>" onchange="s_<?php echo $title; ?>.setMinimum(parseInt(this.value))"/>
+Maximum: <input id="h-max_<?php echo $title; ?>" onchange="s_<?php echo $title; ?>.setMaximum(parseInt(this.value))"/>
+</p>
+
+<script type="text/javascript">
+
+var s_<?php echo $title; ?> = new Slider(document.getElementById("slider-<?php echo $title; ?>"), document.getElementById("slider-input-<?php echo $title; ?>"));
+s_<?php echo $title; ?>.onchange = function () {
+	document.getElementById("h-value_<?php echo $title; ?>").value = s_<?php echo $title; ?>.getValue();
+		document.getElementById("h-min_<?php echo $title; ?>").value = s_<?php echo $title; ?>.getMinimum();
+	document.getElementById("h-max_<?php echo $title; ?>").value = s_<?php echo $title; ?>.getMaximum();
+}
+s.setValue(50);
+
+</script>
+
+<?php
+}
+?>
