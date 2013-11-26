3de58585cddb2fd7560e9ddb0c6d4560abc3622a
JOTAI <jotai.bf.2012@gmail.com>
2013-11-26 01:50:41 -0500

    crear junta
    otro cambio de actualizacion

--- Templates/crear_junta.php
+++ Templates/crear_junta.php
@@ -1,25 +1,35 @@
-<?php
-session_start(); 
-require_once("../require/class.php");
-include ("../includes/slider_horizontal.php");
-
-?>
-
 <html>
 <head>
 <title>>>Nueva Junta<<</title>
+
+<link type="text/css" rel="StyleSheet" href="bluecurve/bluecurve.css" />
 <script type="text/javascript" src="../JavaScript/range.js"></script>
 <script type="text/javascript" src="../JavaScript/timer.js"></script>
 <script type="text/javascript" src="../JavaScript/slider.js"></script>
-<link type="text/css" rel="StyleSheet" href="../Estilos/bluecurve/bluecurve.css" />
-
 </head>
 <body>
-<h1>Creando Nueva Junta</h1>
 
-<?php colocar_slider_horizontal("1",0,100,1); ?>
+<p>Horizontal</p>
+<div class="slider" id="slider-1" tabIndex="1">
+	<input class="slider-input" id="slider-input-1"/>
+</div>
 
+<p>
+Value: <input id="h-value" onchange="s.setValue(parseInt(this.value))"/>
+Minimum: <input id="h-min" onchange="s.setMinimum(parseInt(this.value))"/>
+Maximum: <input id="h-max" onchange="s.setMaximum(parseInt(this.value))"/>
+</p>
 
+<script type="text/javascript">
 
+var s = new Slider(document.getElementById("slider-1"), document.getElementById("slider-input-1"));
+s.onchange = function () {
+	document.getElementById("h-value").value = s.getValue();
+		document.getElementById("h-min").value = s.getMinimum();
+	document.getElementById("h-max").value = s.getMaximum();
+}
+s.setValue(50);
+
+</script>
 </body>
 </html>
