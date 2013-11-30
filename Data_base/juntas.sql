afbbd4c933a1ac65c76812e3903fc6382cc31863
JOTAI <jotai.bf.2012@gmail.com>
2013-11-30 16:35:42 -0500

    hola
    hola

--- Data_base/juntas.sql
+++ Data_base/juntas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 18-11-2013 a las 13:12:00
+-- Tiempo de generación: 30-11-2013 a las 21:34:50
 -- Versión del servidor: 5.5.34-0ubuntu0.13.10.1
 -- Versión de PHP: 5.5.3-1ubuntu2
 
@@ -33,14 +33,16 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `user` varchar(20) DEFAULT NULL,
   `pass` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`cod_cliente`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`cod_cliente`, `nombre`, `num_tarjeta`, `user`, `pass`) VALUES
-(1, 'Juan Basilio', '2147483647', 'JOTA', '123');
+(1, 'Juan Basilio', '2147483647', 'JOTA', '123'),
+(2, 'Alex', '7845681452', 'ALEX', '123'),
+(3, 'Milton', '784512365', 'MILTON', '123');
 
 -- --------------------------------------------------------
 
@@ -55,16 +57,18 @@ CREATE TABLE IF NOT EXISTS `junta` (
   `frec_pago` int(11) DEFAULT NULL,
   `estado` varchar(20) DEFAULT NULL,
   `fecha_ini` date DEFAULT NULL,
+  `cod_cliente` int(11) DEFAULT NULL,
   PRIMARY KEY (`cod_junta`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `junta`
 --
 
-INSERT INTO `junta` (`cod_junta`, `monto_t`, `tiempo_t`, `frec_pago`, `estado`, `fecha_ini`) VALUES
-(1, 1200, 12, 1, 'activo', '2013-11-08'),
-(2, 2400, 12, 1, 'activo', '2013-11-08');
+INSERT INTO `junta` (`cod_junta`, `monto_t`, `tiempo_t`, `frec_pago`, `estado`, `fecha_ini`, `cod_cliente`) VALUES
+(1, 1200, 3, 1, 'activo', '2013-11-08', 1),
+(2, 2400, 12, 1, 'activo', '2013-11-08', 2),
+(3, 900, 9, 1, 'activo', '2013-11-26', 1);
 
 -- --------------------------------------------------------
 
@@ -78,7 +82,16 @@ CREATE TABLE IF NOT EXISTS `tabla_x` (
   `cod_junta` int(11) DEFAULT NULL,
   `puesto` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_tabla_x`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `tabla_x`
+--
+
+INSERT INTO `tabla_x` (`id_tabla_x`, `cod_cliente`, `cod_junta`, `puesto`) VALUES
+(1, 1, 1, 1),
+(2, 2, 1, 3),
+(3, 3, 1, 2);
 
 -- --------------------------------------------------------
 
