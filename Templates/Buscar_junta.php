49ee45ab168dde70c10dbc31f4182adc35fdd7b6
AlexPuente <eljudicador6@gmail.com>
2013-11-30 16:11:21 -0500

    Alex ultimo

--- Templates/Buscar_junta.php
+++ Templates/Buscar_junta.php
@@ -11,21 +11,14 @@ $sentencia = "SELECT cod_junta,monto_t,tiempo_t,frec_pago,estado,fecha_ini from 
 
 	$envio->ejecutar_sentencia($sentencia);
 	$cod_junta = $envio->retornar_array();	
-	echo $cod_junta["cod_junta"];
-	echo $cod_junta["monto_t"];
-	echo $cod_junta["tiempo_t"];
-	echo $cod_junta["frec_pago"];
-	echo $cod_junta["estado"];
-	echo $cod_junta["fecha_ini"];
+	echo "cod_junta: ".$cod_junta["cod_junta"]."/br";
+	echo "monto_t".$cod_junta["monto_t"]."/br";
+	echo "tiempo_t".$cod_junta["tiempo_t"]."/br";
+	echo "frec_pago".$cod_junta["frec_pago"]."/br";
+	echo "estado".$cod_junta["estado"]."/br";
+	echo "fecha_ini".$cod_junta["fecha_ini"]."/br";
 	
 		?>	
-    <form action="xxx.php" method="post" name="form" >
-        <input type="hidden" name="cod_junta" value="<?php echo $cod_junta['cod_junta']; ?>" >
-        <input type="hidden" name="monto_t" value="<?php echo $cod_junta['monto_t']; ?>" >
-        <input type="hidden" name="tiempo_t" value="<?php echo $cod_junta['tiempo_t']; ?>" >
-        <input type="hidden" name="frec_pago" value="<?php echo $cod_junta['frec_pago']; ?>" >
-		<input type="hidden" name="estado" value="<?php echo $cod_junta['estado']; ?>" >
-        <input type="hidden" name="fecha_ini" value="<?php echo $cod_junta['fecha_ini']; ?>" >
-	</form>
+   
     		
 		
