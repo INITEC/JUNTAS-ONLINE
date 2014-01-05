ce57f412379e4dbf7cc937fcbf066ec937b9006e
JOTAI <jotai.bf.2012@gmail.com>
2014-01-04 23:07:44 -0500

    login y registro
    clases y pagina para login y registro v1 ... falta maquetacion

---
+++ require/inicio_class.php
@@ -0,0 +1,38 @@
+<?php 
+require_once ("../require/class.php");
+
+class inicio {
+
+	private $_conexion;
+	private $_verificacion = false;
+	private $_cod_cliente;
+	
+	public function __construct (){
+		$this->_conexion = new conexion();
+	}
+
+	public function verificar_usuario ($user,$pass){
+		$sql = "SELECT user,pass,cod_cliente FROM cliente WHERE user='".$user."' AND pass='".$pass."' ";
+		$this->_conexion->ejecutar_sentencia($sql);
+		if($this->_conexion->tam_respuesta() == 1) { $this->_verificacion = true;
+																	$this->_conexion->ejecutar_sentencia($sql);
+																	$dato = $this->_conexion->retornar_array();
+																	$this->_cod_cliente = $dato["cod_cliente"]; }
+	   else {$this->_verificacion = false;}
+		return $this->_verificacion; 	
+	}
+	
+	public function enviar_registro ($nombre,$num_tarjeta,$user,$pass){
+		$sql = "INSERT INTO `cliente`(`cod_cliente`, `nombre`, `num_tarjeta`, `user`, `pass`) VALUES (null , '".$nombre."' , '".$num_tarjeta."' , '".$user."' , '".$pass."' )";
+		return $this->_conexion->ejecutar_sentencia($sql);
+		}
+
+	public function cod_cliente (){
+		if($this->_verificacion == true) {
+			return $this->_cod_cliente;}
+		else {return 0;}
+	}
+}
+
+?>
+
